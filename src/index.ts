9d2efe273a2b192ae9c1671960107affaaf5729d
Seungku Kim <seungkukim@kaist.ac.kr>
2022-08-19 22:15:22 +0900

    Added CI

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+enum Role {
+    President,
+    VicePresident,
+    Member,
+    Unknown,
+}
+
+export default function getRole(nickname: string): Role {
+    switch (nickname) {
+        case "platypus":
+            return Role.President;
+        case "dora":
+            return Role.VicePresident;
+        case "jeuk":
+            return Role.Member;
+        case "jaewoo":
+            return Role.Unknown;
+        default:
+            return Role.Unknown;
+    }
+}
